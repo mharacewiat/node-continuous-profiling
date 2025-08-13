50e275804e236db31c9e74f069a05efcce063592
Michał Haracewiat <michal.haracewiat@platform.sh>
2025-08-13 14:29:20 +0200

    test

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu
+
+# get internal version
+VERSION=`node -p "require('./package.json').version"`
+
+if [ "$TAG" != "v$VERSION" ]; then
+    echo "ERROR: tag ${TAG} does not match package version ${VERSION}"
+    exit 1
+fi
+
+# npm set //registry.npmjs.org/:_authToken ${NPM_TOKEN}
+# npm publish --access public
+
+npm publish --dry-run
