7e8930c1e1479ad485f3dba2cbda20e0caccda7c
Michał Haracewiat <michal.haracewiat@platform.sh>
2025-08-13 14:29:20 +0200

    test

---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Releasing!"
+
+if [ -n "$NPM_TOKEN" ]; then
+    echo "NPM_TOKEN is set ${NPM_TOKEN}"
+fi
+
+VERSION=`node -p "require('./package.json').version"`
+
+echo "tag ${TAG}"
+echo "version ${VERSION}"
+
+if [ "$TAG" != "v$VERSION" ]; then
+    echo "ERROR: tag ${TAG} does not match package version ${VERSION}"
+    exit 1
+fi
